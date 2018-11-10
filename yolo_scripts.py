63463f6ae97dd9b75c4d3d308ba9e9caf964529b
Luke <lukemarkham1383@gmail.com>
2018-11-10 17:09:09 +0000

    Upload all files

---
+++ yolo_scripts.py
@@ -0,0 +1,65 @@
+from timeit import default_timer as timer
+import numpy as np
+
+from yolo import YOLO
+from PIL import Image
+
+IMAGE = True
+INPUT = r"test_data\VOC2007\JPEGImages\000012.jpg"
+# INPUT = 0
+OUTPUT = "images/"
+
+def detect_video(yolo, video_path, output_path=""):
+    import cv2
+
+    vid = cv2.VideoCapture(video_path)
+    if not vid.isOpened():
+        raise IOError("Couldn't open webcam or video")
+    video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
+    video_fps       = vid.get(cv2.CAP_PROP_FPS)
+    video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                        int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    isOutput = True if output_path != "" else False
+    if isOutput:
+        print("!!! TYPE:", type(output_path), type(video_FourCC), type(video_fps), type(video_size))
+        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
+    accum_time = 0
+    curr_fps = 0
+    fps = "FPS: ??"
+    prev_time = timer()
+    while True:
+        return_value, frame = vid.read()
+        image = Image.fromarray(frame)
+        image = yolo.detect_image(image)
+        result = np.asarray(image)
+        curr_time = timer()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 1
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            fps = "FPS: " + str(curr_fps)
+            curr_fps = 0
+        cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.50, color=(255, 0, 0), thickness=2)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        cv2.imshow("result", result)
+        if isOutput:
+            out.write(result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    yolo.close_session()
+
+def detect_img(yolo, image):
+    print("Analysing Image")
+    img = Image.open(image)
+    r_image = yolo.detect_image(img)
+    r_image.show()
+    yolo.close_session()
+
+if __name__ == '__main__':
+    detect_img(YOLO(r"model_data\tiny\tiny_yolo_weights.h5",
+                    r"test_data\coco_classes.txt"), INPUT)
+
+    # detect_video(YOLO(), INPUT, OUTPUT)
